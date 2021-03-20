ebb71878b378f8a652c033406f5432c0074c24bf
sali73 <sally.elgendy@hotmail.com>
2021-03-19 21:48:10 -0400

    add font size to new fruits form

--- views/New.jsx
+++ views/New.jsx
@@ -12,10 +12,10 @@ class New extends React.Component {
 
               <div style={{ backgroundColor: '#f6f0c4 ', marginTop:'0px', padding:'50px 100px',borderLeft: '5px solid #d99ec9', textAlign: 'center' }}>
                 <form style={{color:'#695787', fontSize:'27px',paddingLeft:'160px'}} action="/fruits" method="POST">
-                    Name: <input style={{marginBottom:'20px', padding:'10px 150px'}} type="text" name="name" /><br/>
-                    Color: <input style={{marginBottom:'20px', padding:'10px 150px'}}  type="text" name="color" /><br/>
-                    Image: <input style={{marginBottom:'20px', padding:'10px 150px'}}  type="text" name="img" /><br/>
-                    Calories: <input style={{marginBottom:'20px', padding:'10px 150px'}}  type="text" name="calories" /><br/>
+                    Name: <input style={{marginBottom:'20px', padding:'10px 150px',fontSize:'17px'}} type="text" name="name" /><br/>
+                    Color: <input style={{marginBottom:'20px', padding:'10px 150px', fontSize:'17px'}}  type="text" name="color" /><br/>
+                    Image: <input style={{marginBottom:'20px', padding:'10px 150px', fontSize:'17px'}}  type="text" name="img" /><br/>
+                    Calories: <input style={{marginBottom:'20px', padding:'10px 150px', fontSize:'17px'}}  type="text" name="calories" /><br/>
                     <span style={{marginLeft:'-320px'}}>Is Ready To Eat: </span><input type="checkbox" name="readyToEat" /><br/>
                     <input style={{ color: ' #f8f9d2', borderRadius:'40%' ,backgroundColor:'#d99ec9' , border:'4px solid #d99ec9', fontSize:'25px',padding:'0px 17px', marginTop:'45px', fontFamily:'Cursive'}}  type="submit" name="" value="Create Fruit"/>
                 </form>
