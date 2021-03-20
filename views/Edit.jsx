53c75b155c563a9dabd996b7b432d22738054eec
sali73 <sally.elgendy@hotmail.com>
2021-03-19 21:18:50 -0400

    style component are done

--- views/Edit.jsx
+++ views/Edit.jsx
@@ -7,29 +7,37 @@ class Edit extends React.Component {
         const { fruit } = this.props;
         return (
             <AppLayout>
-                <h1>Edit Fruit page</h1>
+                <div style={{ textAlign:'center', backgroundColor: '#f6f0c4 ', backgroundImage: 'linear-gradient(315deg, #f6f0c4 74%, #d99ec9 0%)', height:'92px',padding: '10px '}}>
+                 <h2 style={{color:'red',  paddingLeft: '170px', marginTop:'30px', fontSize:'35px' }}>Edit To Fruit </h2>
+                </div>
+                <div style={{ backgroundColor: '#f6f0c4 ', marginTop:'0px', padding:'50px 100px',borderLeft: '5px solid #d99ec9', textAlign: 'center' }}>
+                
                 <form
+                    style={{color:'#695787', fontSize:'27px',paddingLeft:'160px'}}
                     action={`/fruits/edit/${fruit._id}?_method=put`}
                     method="POST"
                 >
-                    Name: <input type="text" name="name" value={fruit.name} />
+                    Fruit Name  <input style={{marginBottom:'20px', padding:'10px 150px', fontSize:'17px'}} type="text" name="name" value={fruit.name} />
                     <br />
-                    Color:{' '}
-                    <input type="text" name="color" value={fruit.color} />
+                    Fruit Color  {'  '}
+                    <input type="text"style={{marginBottom:'20px', padding:'10px 150px',fontSize:'17px'}}  name="color" value={fruit.color} />
                     <br />
-                    <input type="text" name="img" value={fruit.img} />
+                    Fruit Image  {' '}
+                    <input type="text"style={{marginBottom:'20px', padding:'10px 150px', fontSize:'17px'}}  name="img" value={fruit.img} />
                     <br />
-                    <input type="text" name="calories" value={fruit.calories} />
+                    Fruit Calories  {' '}
+                    <input style={{marginBottom:'20px', padding:'10px 150px', fontSize:'17px'}} type="text" name="calories" value={fruit.calories} />
                     <br />
-                    Is Ready To Eat:{' '}
+                    <span style={{marginLeft:'-420px'}}>Is Ready To Eat : {' '}</span>
                     <input
                         type="checkbox"
                         checked={fruit.readyToEat ? 'checked' : ''}
                         name="readyToEat"
                     />
                     <br />
-                    <input type="submit" name="" value="Edit Fruit" />
+                    <input style={{ color: ' #f8f9d2', borderRadius:'40%' ,backgroundColor:'#d99ec9' , border:'4px solid #d99ec9', fontSize:'25px',padding:'0px 17px', marginTop:'45px', fontFamily:'Cursive'}}  type="submit" name="" value="Edit Fruit" />
                 </form>
+                </div>
             </AppLayout>
         );
     }
--- views/users/New.jsx
+++ views/users/New.jsx
@@ -1,18 +1,24 @@
 const React = require('react');
+const AppLayout = require('../AppLayout.jsx');
 
 class NewUser extends React.Component {
     render() {
         return (
-            <div>
-                <h1>New User</h1>
-                <form action="/user/" method="POST">
-                    username: <input type="text" name="username" />
-                    <br />
-                    password: <input type="password" name="password" />
-                    <br />
-                    <input type="submit" value="Create User" />
-                </form>
-            </div>
+            <AppLayout>
+                  <div style={{ textAlign:'center', backgroundColor: '#f6f0c4 ', backgroundImage: 'linear-gradient(315deg, #f6f0c4 74%, #d99ec9 0%)', height:'92px',padding: '10px '}}>
+                 <h2 style={{color:'red',  paddingLeft: '170px', marginTop:'30px', fontSize:'35px' }}>Wellcome Our New User :) </h2>
+                </div>
+                <div style={{ backgroundColor: '#f6f0c4 ', marginTop:'0px', padding:'50px 100px',borderLeft: '5px solid #d99ec9', textAlign: 'center' }}>
+
+                    <form style={{color:'#695787', fontSize:'27px',paddingLeft:'160px',marginTop:'20px' , marginBottom: '20px'}} action="/user/" method="POST">
+                        UserName : <input style={{marginBottom:'20px', padding:'10px 150px', fontSize:'17px'}} type="text" name="username" />
+                        <br />
+                        Password : <input style={{marginBottom:'20px', padding:'10px 150px', fontSize:'17px'}} type="password" name="password" />
+                        <br />
+                        <input style={{ color: ' #f8f9d2', borderRadius:'40%' ,backgroundColor:'#d99ec9' , border:'4px solid #d99ec9', fontSize:'25px',padding:'0px 17px', marginTop:'45px', fontFamily:'Cursive', marginLeft:'30px'}}  type="submit" value="Create User" />
+                    </form>
+                </div>
+            </AppLayout>
         );
     }
 }
